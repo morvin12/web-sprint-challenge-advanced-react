fb88a2b4667af86f1856533bbf26b01ac6349906
Michael Orvin <morvin1212@gmail.com>
2021-12-02 20:35:29 -0700

    Fixed plural errors

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,7 +1,18 @@
 // write your custom hook here to control your checkout form
 import { useState } from 'react';
 
-const useForm = () => {
+const useForm = (initialValues) => {
+
+    const [value, setValue] = useState(initialValues);
+
+    const handleChanges = (e) => {
+        setValue({ 
+            ...value, 
+            [e.target.name]: e.target.value 
+        });
+      };
+
+    return [value, handleChanges];
 
 }
 
