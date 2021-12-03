49d3ef4ca0eecbec59ee29c5d6c0f0aaeb113956
Michael Orvin <morvin1212@gmail.com>
2021-12-03 09:44:19 -0700

    Fixed errors on form test, and completed MVP

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -12,4 +12,27 @@ test("renders without errors", () => {
 
 test("shows success message on submit with form details", () => {
     render(<CheckoutForm/>);
+    const firstName = screen.getByLabelText(/First Name:/i);
+    userEvent.type(firstName, "Harry");
+
+    const lastName = screen.getByLabelText(/Last Name/i);
+    userEvent.type(lastName, "Potter");
+
+    const address = screen.getByLabelText(/Address/i);
+    userEvent.type(address, "Hogwarts");
+
+    const city = screen.getByLabelText(/City/i);
+    userEvent.type(city, "Wizard World");
+
+    const state = screen.getByLabelText(/State/i);
+    userEvent.type(state, "Florida");
+
+    const zip = screen.getByLabelText(/Zip/i);
+    userEvent.type(zip, "32819");
+
+    const button = screen.getByRole('button');
+    userEvent.click(button);
+
+    const successMsg = screen.getByTestId(/successMessage/i);
+    expect(successMsg).toBeInTheDocument();
 });
