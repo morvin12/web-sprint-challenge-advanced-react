654ea1e0e4a7725a97e54c8c4b063ff5df0857fd
Michael Orvin <morvin1212@gmail.com>
2021-12-03 08:53:39 -0700

    Finished ReadMe questions

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -2,6 +2,7 @@ import React from "react";
 import MutationObserver from 'mutationobserver-shim';
 import { render, screen } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import userEvent from "@testing-library/user-event";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
